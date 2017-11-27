16683c02014f20f3148d230ff0a1c3f306f13f96
alanikaev <ikaeffalan3@gmail.com>
2017-11-27 22:25:00 +0300

    Readiness: 95%

--- Mart/blocks/slider/js/slider.js
+++ Mart/blocks/slider/js/slider.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.slider').slick({
   	arrows: true,
   	mobileFirst: true,
- //  	nextArrow:'<div class="slider-button left"><div class="slider-button__arrow right"></div></div>',
-	// prevArrow:'<div class="slider-button right"><div class="slider-button__arrow left"></div></div>',
+  	nextArrow:'<div class="slider-button left"><div class="slider-button__arrow right"></div></div>',
+	prevArrow:'<div class="slider-button right"><div class="slider-button__arrow left"></div></div>',
   });
 });
