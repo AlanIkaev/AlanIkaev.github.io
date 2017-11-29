a50059a18740f3857707303ffd612eb474b9ee7f
alanikaev <ikaeffalan3@gmail.com>
2017-11-29 11:04:55 +0300

    Readiness:98%(minor changes)

--- Mart/blocks/slider/js/slider.js
+++ Mart/blocks/slider/js/slider.js
@@ -4,5 +4,21 @@ $(document).ready(function(){
   	mobileFirst: true,
   	nextArrow:'<div class="slider-button left"><div class="slider-button__arrow right"></div></div>',
 	prevArrow:'<div class="slider-button right"><div class="slider-button__arrow left"></div></div>',
+	responsive: [
+		{
+			breakpoint: 300,
+			settings: {
+				dots: true,
+				arrows: false
+			}
+		},
+		{
+			breakpoint: 1023,
+			settings: {
+				dots: false,
+				arrows: true
+			}
+		}
+	]
   });
 });
