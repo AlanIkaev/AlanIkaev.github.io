30eb4aa2222350cc78d1e5265eb3c1d6225ca36a
Alan Ikaev <ikaeffalan3@gmail.com>
2018-01-23 23:35:54 +0300

    update + drumkit

--- Mart/gulpfile.js
+++ Mart/gulpfile.js
@@ -3,3 +3,25 @@
 const gulp = require("gulp4");
 const less = require("gulp-less");
 const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const del = require("del");
+
+gulp.task("less",function(){
+	return gulp.src('blocks/**/*.less')
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(sourcemaps.write())
+		.pipe(concat("all.css"))
+		.pipe(gulp.dest('public'));
+});
+
+gulp.task("img", function(){
+	return gulp.src('blocks/{about,best-sellers,contact,discount,featured-products,latest-news,menu-section,new-collection,newsletter,partners,reviews,slider,top-header}/img/*.*', {base: 'blocks/{about,best-sellers,contact,discount,featured-products,latest-news,menu-section,new-collection,newsletter,partners,reviews,slider,top-header}/img/'})
+		.pipe(gulp.dest('./public/img'));
+})
+
+gulp.task('clean', function(){
+	return del('public');
+});
+
+gulp.task('build', gulp.series('clean','less'));
