bf1b2a7d9183cf97c8e64f234f5066e22342cd9f
Alan Ikaev <ikaeffalan3@gmail.com>
2018-02-12 23:30:53 +0300

    add burger animation + styling scrollbar

--- Spedo/blocks/mobile-menu/mobile-menu.js
+++ Spedo/blocks/mobile-menu/mobile-menu.js
@@ -2,6 +2,6 @@ var burger = document.querySelector(".header__burger");
 var menu = document.querySelector(".mobile-menu");
 
 burger.onclick = function(){
+  burger.classList.toggle("is-active");
   menu.classList.toggle("mobile-menu--open");
-
 }
