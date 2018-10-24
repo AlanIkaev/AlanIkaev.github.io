2ce0a286c4d76016ea525a13dc4700565ad6bffb
alan <ikaeffalan3@gmail.com>
2018-10-24 23:30:40 +0300

    Add all sliders

--- Miushi/global-js/slider.js
+++ Miushi/global-js/slider.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 	    arrows: true,
 	  	mobileFirst: true,
 	  	slidesToShow: 1,
+	  	autoplay: true,
+	  	autoplaySpeed: 2000,
 	  	lazyLoad: 'progressive',
 	  	nextArrow: '<div class="slider-button right"><span class="arrow right"></span></div>',
 	  	prevArrow: '<div class="slider-button left"><span class="arrow left"></span></div>',
@@ -26,6 +28,134 @@ $(document).ready(function(){
   	});
 
 	$('.sets-slider').slick({
+	  slidesToShow: 1,
+	  arrows: false,
+	  dots: true,
+	  mobileFirst: true,
+	  variableWidth: true,
+	  responsive:[
+	  	 {
+	  	 	breakpoint:300,
+	  	 	settings:{
+	  	 		slidesToShow: 1,
+	  	 		centerMode: true,
+	  	 		dots: false
+	  	 	}
+	  	 },
+
+	  	 {
+	  	 	breakpoint: 750,
+	  	 	settings:{
+	  	 		slidesToShow: 3,
+	  	 		centerMode: false
+	  	 	}
+	  	 },
+	  	 {
+	  	 	breakpoint: 1170,
+	  	 	settings:{
+	  	 		slidesToShow: 1,
+	  	 	}
+	  	 }
+	  ]
+	});
+
+	$('.rolls-slider').slick({
+	  slidesToShow: 1,
+	  arrows: false,
+	  dots: true,
+	  mobileFirst: true,
+	  variableWidth: true,
+	  responsive:[
+	  	 {
+	  	 	breakpoint:300,
+	  	 	settings:{
+	  	 		slidesToShow: 1,
+	  	 		centerMode: true,
+	  	 		dots: false
+	  	 	}
+	  	 },
+
+	  	 {
+	  	 	breakpoint: 750,
+	  	 	settings:{
+	  	 		slidesToShow: 3,
+	  	 		centerMode: false
+	  	 	}
+	  	 },
+	  	 {
+	  	 	breakpoint: 1170,
+	  	 	settings:{
+	  	 		slidesToShow: 1,
+	  	 	}
+	  	 }
+	  ]
+	});
+
+	$('.pizza-slider').slick({
+	  slidesToShow: 1,
+	  arrows: false,
+	  dots: true,
+	  mobileFirst: true,
+	  variableWidth: true,
+	  responsive:[
+	  	 {
+	  	 	breakpoint:300,
+	  	 	settings:{
+	  	 		slidesToShow: 1,
+	  	 		centerMode: true,
+	  	 		dots: false
+	  	 	}
+	  	 },
+
+	  	 {
+	  	 	breakpoint: 750,
+	  	 	settings:{
+	  	 		slidesToShow: 3,
+	  	 		centerMode: false
+	  	 	}
+	  	 },
+	  	 {
+	  	 	breakpoint: 1170,
+	  	 	settings:{
+	  	 		slidesToShow: 1,
+	  	 	}
+	  	 }
+	  ]
+	});
+
+	$('.wok-slider').slick({
+	  slidesToShow: 1,
+	  arrows: false,
+	  dots: true,
+	  mobileFirst: true,
+	  variableWidth: true,
+	  responsive:[
+	  	 {
+	  	 	breakpoint:300,
+	  	 	settings:{
+	  	 		slidesToShow: 1,
+	  	 		centerMode: true,
+	  	 		dots: false
+	  	 	}
+	  	 },
+
+	  	 {
+	  	 	breakpoint: 750,
+	  	 	settings:{
+	  	 		slidesToShow: 3,
+	  	 		centerMode: false
+	  	 	}
+	  	 },
+	  	 {
+	  	 	breakpoint: 1170,
+	  	 	settings:{
+	  	 		slidesToShow: 1,
+	  	 	}
+	  	 }
+	  ]
+	});
+
+	$('.offers-slider').slick({
 	  slidesToShow: 1,
 	  arrows: false,
 	  dots: true,
